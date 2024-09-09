2f3905eaa98003d755f77a1827efa0c724afbd2b
Leif Lundberg <leif-lundberg@outlook.com>
2024-09-09 22:54:49 +0200

    first commit

---
+++ Data/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace DragDrop.Data
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsNew { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+using DragDrop;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Plk.Blazor.DragDrop;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazorDragDrop();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
